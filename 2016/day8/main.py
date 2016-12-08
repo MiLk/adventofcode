80e24bb3431eae72759a82cec9afaaa8ca52a517
Emilien Kenler <emilien@kkvesper.jp>
2016-12-08 15:04:51 +0900

    Cleanup

--- 2016/day8/main.py
+++ 2016/day8/main.py
@@ -18,8 +18,8 @@ def process_line(screen, line):
             for j in xrange(0, int(a)):
                 screen[i][j] = '#'
     elif instruction == 'rotate':
-        (arg0, arg1, arg3, arg4, shift) = line.split(' ')
-        (arg3label, index) = arg3.split('=')
+        (_, arg1, arg3, _, shift) = line.split(' ')
+        (_, index) = arg3.split('=')
         index = int(index)
         shift = int(shift)
         if arg1 == 'row':
@@ -29,8 +29,7 @@ def process_line(screen, line):
         elif arg1 == 'column':
             tmp = [list(screen[i]) for i in xrange(0, 6)]
             for i in xrange(0, 6):
-                old = (i-shift+6) % 6
-                screen[i][index] = tmp[old][index]
+                screen[i][index] = tmp[(i-shift+6) % 6][index]
 
     return screen
 
@@ -38,17 +37,12 @@ def process_line(screen, line):
 def main():
     lines = readinput(sys.argv[1])
 
-    screen = [[' '] * 50 for i in range(0, 6)]
+    screen = [list(x) for x in [[' '] * 50] * 6]
 
     for line in lines:
         screen = process_line(screen, line.strip())
 
-    c = 0
-    for i in xrange(0, 6):
-        for j in xrange(0, 50):
-            if screen[i][j] == '#':
-                c += 1
-
+    c = reduce(lambda acc, line: acc + line.count('#'), screen, 0)
     print('Step 1: %d' % c)
 
     print('Step 2:')
