e1f97649c90d1658a002475cb124beed7cf1602e
MiLk <hello@emilienkenler.com>
2016-12-18 14:56:39 +0900

    Day 18: Like a Rogue

--- 2016/day18/main.py
+++ 2016/day18/main.py
@@ -10,18 +10,9 @@ def readinput(path):
 
 
 def get_tile(pos, previous):
-    left, center, right = ('.', previous[pos], '.')
-    if pos > 0:
-        left = previous[pos - 1]
-    if pos < len(previous) - 1:
-        right = previous[pos + 1]
-    traps = [
-        ('^', '^', '.'),
-        ('.', '^', '^'),
-        ('^', '.', '.'),
-        ('.', '.', '^'),
-    ]
-    return '^' if (left, center, right) in traps else '.'
+    left = previous[pos - 1] if pos > 0 else '.'
+    right = previous[pos + 1] if pos < len(previous) - 1 else '.'
+    return '^' if left != right else '.'
 
 
 def main():
