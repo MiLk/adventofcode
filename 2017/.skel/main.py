494540fa66762557a8472a0dd21bd4ecdf7c5083
MiLk <hello@emilienkenler.com>
2017-12-03 16:23:10 +0900

    Update skeleton

--- 2017/.skel/main.py
+++ 2017/.skel/main.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 import sys
 
 
-def readinput(path):
+def read_input(path):
     if not path:
         path = 'input.txt'
     with open(path) as f:
@@ -18,13 +18,24 @@ def process_line(line):
     return line
 
 
+def p1(line):
+    return line
+
+
+def p2(line):
+    return line
+
+
 def main():
-    lines = readinput(sys.argv[1])
+    path = sys.argv[1] if len(sys.argv) > 1 else None
+    lines = read_input(path)
     lines = map(process_line, lines)
 
-    print('Part 1: %d' % 0)
+    s1 = map(p1, lines)
+    print('Part 1: %d' % s1)
 
-    print('Part 2: %d' % 0)
+    s2 = map(p2, lines)
+    print('Part 2: %d' % s2)
 
 
 if __name__ == "__main__":
