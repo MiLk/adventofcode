756cc0fbca3e9cd484b93554768b7f5cf724c941
Emilien Kenler <emilien@kkvesper.jp>
2018-12-03 15:54:57 +0900

    Refactor

--- 2018/day3/main.py
+++ 2018/day3/main.py
@@ -18,37 +18,36 @@ def claim(line):
     return n, int(l), int(t), int(w), int(h)
 
 
-def p1m(claims):
+def p1dict(claims):
     fabric = defaultdict(int)
     for _, l, t, w, h in claims:
         for i, j in itertools.product(range(l, l+w), range(t, t+h)):
             fabric[(i, j)] += 1
-    return sum(1 if v >= 2 else 0 for v in fabric.values())
+    return sum(v >= 2 for v in fabric.values())
 
 
-def p1(claims):
+def p1set(claims):
     claimed = set()
     overclaimed = set()
     for _, l, t, w, h in claims:
-        for i, j in itertools.product(range(l, l+w), range(t, t+h)):
-            if (i, j) in claimed:
-                overclaimed.add((i, j))
-            claimed.add((i, j))
+        current = set(itertools.product(range(l, l+w), range(t, t+h)))
+        overclaimed |= (current & claimed)
+        claimed |= current
     return len(overclaimed)
 
 
 def p2(claims):
-    claimed = dict()
+    claimed = defaultdict(set)
     overclaimed = set()
     ids = set()
     for n, l, t, w, h in claims:
         ids.add(n)
         for i, j in itertools.product(range(l, l+w), range(t, t+h)):
-            if len(claimed.get((i, j), set())) > 0:
-                overclaimed = overclaimed | {n} | claimed[(i, j)]
-            claimed[(i, j)] = claimed.get((i, j), set()) | {n}
+            if len(claimed[(i, j)]) > 0:
+                overclaimed |= {n} | claimed[(i, j)]
+            claimed[(i, j)] |= {n}
     return ids - overclaimed
 
 
 if __name__ == "__main__":
-    solve(sys.argv, claim, p1, p2)
+    solve(sys.argv, claim, p1set, p2)
