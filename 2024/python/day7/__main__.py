36c52477bbdffc453e39be020b519900362c3051
Emilien Kenler <hello@emilienkenler.com>
2024-12-09 10:12:18 +0900

    2024: Day 7 - typing

--- 2024/python/day7/__main__.py
+++ 2024/python/day7/__main__.py
@@ -25,31 +25,31 @@ def is_solvable(value: int, numbers: list[int]) -> bool:
 
 equations = list(map(parse_line, read_input(__package__)))
 
-# print("Part 1:", sum(value for value, numbers in equations if is_solvable(value, numbers)))
+print("Part 1:", sum(value for value, numbers in equations if is_solvable(value, numbers)))
 
 
-def fill_operators(numbers: list[int]) -> Iterable[list[int | str]]:
+def fill_operators(numbers: list[int]) -> Iterable[list[tuple[int, str]]]:
     if len(numbers) == 1:
-        yield [numbers[0]]
+        yield [(numbers[0], "")]
         return
-    heads, tail = numbers[:-1], numbers[-1]
-    for filled in list(fill_operators(heads)):
-        yield filled + ["+", tail]
-        yield filled + ["*", tail]
-        yield filled + ["||", tail]
+    head, tail = numbers[0], numbers[1:]
+    for filled in list(fill_operators(tail)):
+        yield [(head, "+")] + filled
+        yield [(head, "*")] + filled
+        yield [(head, "||")] + filled
 
 
-def evaluate(expression: list[int | str]) -> int:
+def evaluate(expression: list[tuple[int, str]]) -> int:
     if len(expression) == 1:
-        return expression[0]
-    first, operator, second, tail = expression[0], expression[1], expression[2], expression[3:]
+        return expression[0][0]
+    (first, operator), (second, remaining_op), tail = expression[0], expression[1], expression[2:]
     match operator:
         case "+":
-            return evaluate([first + second] + tail)
+            return evaluate([(first + second, remaining_op)] + tail)
         case "*":
-            return evaluate([first * second] + tail)
+            return evaluate([(first * second, remaining_op)] + tail)
         case "||":
-            return evaluate([int(f"{first}{second}")] + tail)
+            return evaluate([(int(f"{first}{second}"), remaining_op)] + tail)
         case _:
             raise NotImplementedError
 
